0523ec3daf1077ac4f708e54ece6b239a6f9ab2f
surajmt8848 <you@example.com>
2021-05-02 03:16:34 +0545

    add detail view

--- hamro-dokan-vue/src/views/Home.vue
+++ hamro-dokan-vue/src/views/Home.vue
@@ -1,11 +1,69 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome, to Hamro Dokan..</p>
+        <p class="subtitle">Hami sabaiko dokan, Hamro Dokan..</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail" alt="product thumbnail" />
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">
+            <span>&#8360; {{ product.price }}</span>
+          </p>
+          <router-link
+            v-bind:to="product.get_absolute_url"
+            class="button is-dark mt-4"
+            >View</router-link
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   components: {},
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
+  mounted() {
+    this.getLatestProducts();
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get("latest-products/")
+        .then((res) => {
+          this.latestProducts = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
 };
 </script>
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
---
+++ hamro-dokan-vue/src/views/product/Product.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="page-product">
+    <div class="columns is-multiline">
+      <div class="column is-9">
+        <figure class="image mb=6">
+          <img v-bind:src="product.get_image" alt="" />
+        </figure>
+        <h1 class="title">{{ product.title }}</h1>
+        <p>{{ product.description }}</p>
+      </div>
+      <div class="column is-3">
+        <h2 class="subtitle">Information</h2>
+        <p><strong>Price: </strong>Rs {{ product.price }}</p>
+
+        <div class="field has-addons mt-6">
+          <div class="control">
+            <input type="number" class="input" min="1" v-model="quantity" />
+          </div>
+          <div class="control">
+            <a href="" class="buttoon is-dark">Add to cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Product",
+  data() {
+    return {
+      product: {},
+      quantity: 1,
+    };
+  },
+
+  mounted() {
+    this.getProduct();
+  },
+  methods: {
+    getProduct() {
+      const category_slug = this.$route.params.category_slug;
+      const product_slug = this.$route.params.product_slug;
+
+      axios
+        .get(`product/${category_slug}/${product_slug}`)
+        .then((res) => {
+          this.product = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
