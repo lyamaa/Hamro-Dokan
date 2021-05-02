bc4b1f0c95f13b818733cbb01d9413ed071202e4
surajmt8848 <you@example.com>
2021-05-02 15:00:12 +0545

    cart setup in store

--- hamro-dokan-vue/src/store/index.js
+++ hamro-dokan-vue/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
@@ -6,13 +6,29 @@ export default createStore({
       items: [],
     },
     isAuthenticated: false,
-    token: '',
-    isLoading: false
+    token: "",
+    isLoading: false,
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem("cart")) {
+        state.cart = JSON.parse(localStorage.getItem("cart"));
+      } else {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(
+        (i) => i.product.id === item.product.id
+      );
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
